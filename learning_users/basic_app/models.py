f135bb64dd2eb8321a209a07387e25873b0debb1
Aakash <aakashverma51805@gmail.com>
2020-10-11 00:37:17 +0530

    'first'

---
+++ learning_users/basic_app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class UserProfileInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # additional items to be added in form other than those User have
+    # not required
+    portfolio_site = models.URLField(blank=True)
+    profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+
+    def __str__(self):
+        return self.user.username
